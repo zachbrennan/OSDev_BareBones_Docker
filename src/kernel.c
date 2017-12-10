8f4faf30169952e28bb0805d858ccd05748d9a98
Zach <zbrennan613@gmail.com>
2017-12-10 11:33:07 -0500

    added tab and \t support, changed color to green

--- src/kernel.c
+++ src/kernel.c
@@ -15,7 +15,7 @@
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
- 
+
 /* Hardware text mode color constants. */
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
@@ -36,6 +36,10 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
  
+/* Function Prototypes */
+void terminal_writestring(const char* data);
+/* End Prototypes */
+
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
 	return fg | bg << 4;
 }
@@ -62,7 +66,7 @@ uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -85,6 +89,9 @@ void terminal_putchar(char c) {
     if (c == '\n') {
         terminal_row++;
         terminal_column = 0;
+    }
+    else if (c == '\t') {
+        terminal_writestring("    ");
     }
     else {
     	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
@@ -113,5 +120,5 @@ void kernel_main(void) {
 	terminal_initialize();
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\nThis is a new line!");
+	terminal_writestring("Hello,	kernel World!\nThis is\ta new line!");
 }
